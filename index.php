51dec9eb15596c1f9ebd00e6433242c4fc6b56a2
Denis Karakchiev <uokersam@gmail.com>
2018-12-02 20:59:36 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Eizenbanum</h1>
+<div>
+    Здесь планируется создать очередной to-do list =)
+    <p>
+        <span>
+        <?php
+        
+echo date('d.M.Y  h:i'); ?></span>
+    </p>
+</div>
+
+    <?php
+
+$textVar = 'hello php world!';
+
+echo $textVar;
+echo '<br>';
+echo 13 * 13;
+
+?>
+</body>
+</html>
