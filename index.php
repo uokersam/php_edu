9065be0a1dbea6120bfb415bf655850910cf78b9
uokersampoz <uokersampoz@gmail.com>
2018-12-06 18:46:20 +0300

    connecting postgres

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles.css"/>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Eizenbanum</title>
+  <link rel="stylesheet" href="styles.css"/>
 </head>
 <body>
 <h1>Eizenbanum</h1>
 <?php echo 'php is working' ?>
 
-<div class="pigeon"> and styles too!</div>
-<form action="/index.php" method="GET">
-<input type="text" placeholder='Say something' name="testinput">
-<button>Press me</button>
-<?php
-if (isset($_GET['testinput'])) {
-    echo 'yeap, $_GET[] is getting parameters =) <br> and you say: ' . $_GET['testinput'];
-}
+<ul>
+    <?php
+    $host = 'localhost';
+    $user = 'eadmin';
+    $pass = 'admin';
+    $db = 'edb';
 
-?>
+    $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+    or die ("Could not connect to server\n");
+
+    $query = "SELECT * FROM  list";
+    $rs = pg_query($con, $query) or die("Cannot execute query: $query\n");
+
+    while ($row = pg_fetch_row($rs)) {
+        $Arr[] = $row[0];
+    }
+    pg_close($con);
+    echo json_encode($Arr);
+    ?>
+</ul>
+
+<form action="/script.php" method="POST">
+  <input type="text" placeholder="New li elem" name="newElement">
+  <button>Add</button>
 </form>
 
 <script src="script.js"></script>
---
+++ script.php
@@ -0,0 +1,7 @@
+<?php
+$el = $_POST['newElement'];
+
+
+
+//todo: add element to the database,
+// location to index.php
