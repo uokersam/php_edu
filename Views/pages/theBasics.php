3d3b0727d7ebcee8ab69fb437ced1a743ab707f5
Denis Karakchiev <uokersam@gmail.com>
2019-02-07 16:44:14 +0300

    configuring PHP CS

--- Views/pages/theBasics.php
+++ Views/pages/theBasics.php
@@ -1,2 +1,4 @@
 <?php
-var_dump($_SERVER['PATH_INFO']); // if url accessed by http://localhost:8000/Views/pages/theBasics.php/foo/bar then value is '/foo/bar'
+
+// if url accessed by http://localhost:8000/Views/pages/theBasics.php/foo/bar then value is '/foo/bar'
+var_dump($_SERVER['PATH_INFO']);
