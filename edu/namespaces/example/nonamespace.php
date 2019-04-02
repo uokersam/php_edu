58b0b31f37eaf274f3711acb8e0fae1306b250b8
Denis Karakchiev <uokersam@gmail.com>
2019-04-02 14:23:28 +0300

    nonamespace.php

---
+++ edu/namespaces/example/nonamespace.php
@@ -0,0 +1,20 @@
+<?php
+class classname
+{
+    function __construct()
+    {
+        echo __METHOD__,"\n";
+    }
+}
+function funcname()
+{
+    echo __FUNCTION__,"\n";
+}
+const constname = "global";
+
+$a = 'classname';
+$obj = new $a; // выводит classname::__construct
+$b = 'funcname';
+$b(); // выводит funcname
+echo constant('constname'), "\n"; // выводит global
+?>
