197e75a26762ba09d8ddc46c6d0433f42f7145ab
Denis Karakchiev <uokersam@gmail.com>
2019-04-02 14:23:43 +0300

    withnamespace.php

---
+++ edu/namespaces/example/withnamespace.php
@@ -0,0 +1,35 @@
+<?php
+namespace namespacename;
+class classname
+{
+    function __construct()
+    {
+        echo __METHOD__,"\n";
+    }
+}
+function funcname()
+{
+    echo __FUNCTION__,"\n";
+}
+const constname = "namespaced";
+
+include 'example1.php';
+
+$a = 'classname';
+$obj = new $a; // выводит classname::__construct
+$b = 'funcname';
+$b(); // выводит funcname
+echo constant('constname'), "\n"; // выводит global
+
+/* обратите внимание, что при использовании двойных кавычек символ обратного слеша должен быть заэкранирован. Например, "\\namespacename\\classname" */
+$a = '\namespacename\classname';
+$obj = new $a; // выводит namespacename\classname::__construct
+$a = 'namespacename\classname';
+$obj = new $a; // также выводит namespacename\classname::__construct
+$b = 'namespacename\funcname';
+$b(); // выводит namespacename\funcname
+$b = '\namespacename\funcname';
+$b(); // также выводит namespacename\funcname
+echo constant('\namespacename\constname'), "\n"; // выводит namespaced
+echo constant('namespacename\constname'), "\n"; // также выводит namespaced
+?>
